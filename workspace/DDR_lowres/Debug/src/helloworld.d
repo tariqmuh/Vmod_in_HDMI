206e45414fa3cc3cbb0370221e0a4e03715cbf84
tariqmuh <khurram_tariq@live.ca>
2014-02-20 15:33:21 -0500

    Adding all files

---
+++ workspace/DDR_lowres/Debug/src/helloworld.d
@@ -0,0 +1,16 @@
+src/helloworld.d: ../src/helloworld.c \
+ ../../standalone_bsp_0/microblaze_0/include/xparameters.h \
+ ../../standalone_bsp_0/microblaze_0/include/xbasic_types.h \
+ ../../standalone_bsp_0/microblaze_0/include/xutil.h \
+ ../../standalone_bsp_0/microblaze_0/include/xbasic_types.h \
+ ../../standalone_bsp_0/microblaze_0/include/xstatus.h
+
+../../standalone_bsp_0/microblaze_0/include/xparameters.h:
+
+../../standalone_bsp_0/microblaze_0/include/xbasic_types.h:
+
+../../standalone_bsp_0/microblaze_0/include/xutil.h:
+
+../../standalone_bsp_0/microblaze_0/include/xbasic_types.h:
+
+../../standalone_bsp_0/microblaze_0/include/xstatus.h:
---
+++ workspace/hello_world_0/Debug/src/helloworld.d
@@ -0,0 +1,16 @@
+src/helloworld.d: ../src/helloworld.c \
+ ../../hello_world_bsp_0/microblaze_0/include/xparameters.h \
+ ../../hello_world_bsp_0/microblaze_0/include/xbasic_types.h \
+ ../../hello_world_bsp_0/microblaze_0/include/xutil.h \
+ ../../hello_world_bsp_0/microblaze_0/include/xbasic_types.h \
+ ../../hello_world_bsp_0/microblaze_0/include/xstatus.h
+
+../../hello_world_bsp_0/microblaze_0/include/xparameters.h:
+
+../../hello_world_bsp_0/microblaze_0/include/xbasic_types.h:
+
+../../hello_world_bsp_0/microblaze_0/include/xutil.h:
+
+../../hello_world_bsp_0/microblaze_0/include/xbasic_types.h:
+
+../../hello_world_bsp_0/microblaze_0/include/xstatus.h:
